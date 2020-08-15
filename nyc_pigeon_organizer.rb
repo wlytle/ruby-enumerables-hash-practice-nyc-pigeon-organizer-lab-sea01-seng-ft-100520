2235d524a8226efed008f259290f7c976bd82093
Will Lytle <wlytle89@gmail.com>
2020-08-15 22:13:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,9 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # Original hashes are `:color`, `:gender`, and `:lives`, with sub hashes of categories and values of names
+  # need hashes of names with sub hashes of the above categories and values of categories
+  
+  internal_keys = pigeon_data.map { |key,value| key }
+  0...internal_keys.length 
+  values = pigeon_data[internal_keys[0]].map { |key,value| key }
+  
 end
